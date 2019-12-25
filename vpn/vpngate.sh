ec4b725588b134742bd8513389f1e66edb8cd705
Ashutosh Varma <ashutoshvarma11@live.com>
2019-12-25 10:34:17 +0530

    rewrite _connect_vpn to handle openvpn service

--- vpn/vpngate.sh
+++ vpn/vpngate.sh
@@ -1,16 +1,18 @@
 PORT_TIMEOUT=2
 VPNGATE_API="http://www.vpngate.net/api/iphone/"
 
-AUTH_FILE=$(pwd)'/vpnauth.txt'
-OPENVPN_DIR="/etc/openvpn"
+OPENVPN_DIR="/etc/openvpn/client"
+OPENVPN_CLIENT_SERVICE="openvpn-client"
 
+AUTH_FILE=${OPENVPN_DIR}/'vpnauth'
 CACHE_VPN='vpn.cache'
 CACHE_TIMEOUT_DAYS=1
 
 function _mkauthfile(){
     if ! [ -s $AUTH_FILE ]; then
-        echo "vpn" > $AUTH_FILE
-        echo "vpn" >> $AUTH_FILE
+        # echo "vpn" > $AUTH_FILE
+        # echo "vpn" >> $AUTH_FILE
+        printf "vpn\nvpn" | sudo tee $AUTH_FILE >/dev/null
     fi
 }
 
@@ -52,22 +54,71 @@ function _check_vpn(){
     fi
 }
 
+function kill_openvpn(){
+    sudo killall -SIGINT openvpn
+    # sudo systemctl stop ${OPENVPN_CLIENT_SERVICE}@"$1" 
+}
 
-function connect_vpn(){
+function is_openvpn_running(){
+    pgrep openvpn || return 1
+    # sudo systemctl status ${OPENVPN_CLIENT_SERVICE}@"$1" --no-pager
+}
+
+function _connect_vpn(){
+    local vpn=("$@")
+    local open_config=$(_decode ${vpn[14]})
+    local vpn_name=${vpn[0]}-${vpn[6]}
+    local config_file=${OPENVPN_DIR}/${vpn_name}.conf
+
+
+    # save openvpn config to /etc/openvpn/ (not using redirection cuz it will lead to permission error.)
+    sed "s/#auth-user-pass/auth-user-pass ${AUTH_FILE//\//\\/}/g" <<< "$open_config" | sudo tee ${config_file} &>/dev/null
+    
+    if is_openvpn_running "$vpn_name";then
+        kill_openvpn "$vpn_name" && (echo "Cannot kill running OpenVPN process. Retry after killing all instances of openvpn"; exit 1)
+    else
+        echo NOVPN RUNNING
+    fi
+
+    echo "Trying to connect to ${vpn_name}."
+
+    sudo systemctl start ${OPENVPN_CLIENT_SERVICE}@${vpn_name}
+
+    # Our Logic to Test if OpenVPN connects sucessfully:_
+    # - Check every n seconds for openvpn service status, if not running 
+    #   then it must have failed. 
+    # - If even after N seconds it is running then it must have succeded.
+    #NOTE:- This is an extremely bad logic, CHANGE IT.
+    for i in {1..4}; do
+        if ! is_openvpn_running; then
+            return 1
+        fi
+        sleep 1.5s
+        echo i
+    done
+    return 0
+
+    # if is_openvpn_running; then
+    #     echo "Connected to ${vpn_name}."
+    # else
+    #     echo "Failed to Connect to ${vpn_name}"
+    #     return 1
+    # fi
+}
+
+
+function vpn(){
     while read -r line; do
         IFS="," 
         local vpn=($line)
         if _check_vpn ${vpn[14]}; then
-            _mkauthfile
-            local open_config=$(_decode ${vpn[14]})
-            # save openvpn config to /etc/openvpn/ (not using redirection cuz it will lead to permission error.)
-            sed "s/#auth-user-pass/auth-user-pass ${AUTH_FILE//\//\\/}/g" <<< "$open_config" | sudo tee ${OPENVPN_DIR}/${vpn[0]}-${vpn[6]}.ovn &>/dev/null
             echo "${vpn[0]}-${vpn[6]} : Working"
-            exit 0
+            _connect_vpn "${vpn[@]}" && exit 0
         else
             echo "${vpn[0]}-${vpn[6]} : Not Working"
         fi
     done <<< $(_getcsv)
 }
 
-connect_vpn
+_mkauthfile
+vpn
